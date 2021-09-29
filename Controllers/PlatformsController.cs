7eb1ca7587b9b65045f46a3acb0039226455df88
Stephanie Miranda <smira6891@live.com>
2021-09-29 10:00:50 -0600

    This complete the CR portions of CRUD.

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -34,5 +34,33 @@ namespace PlatformService.Controllers
 
                return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
           }
+
+          [HttpGet("{id}", Name = "GetPlatformById")]
+          public ActionResult<PlatformReadDto> GetPlatformById(int id)
+          {
+               var platformitem = _repository.GetPlatformById(id);
+
+               if(platformitem != null){
+                    return Ok(_mapper.Map<PlatformReadDto>(platformitem));
+               }
+
+               return NotFound();
+          }
+
+          [HttpPost]
+          public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+          {
+               //if we were updating, we'd need to do more, but in this case, we're just adding something.
+               var platformModel = _mapper.Map<Platform>(platformCreateDto);
+               _repository.CreatePlatform(platformModel);
+               _repository.SaveChanges();
+
+               //pass back success, if it is successful: 201, URI, and 
+               var PlatformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+               //returns the HTTP 201
+               return CreatedAtRoute(nameof(GetPlatformById), new {Id = PlatformReadDto.Id}, PlatformReadDto);
+
+          }
      }
 }
